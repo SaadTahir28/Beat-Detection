6340cf0e16709b35aefa21f4f92c8fbe7498eadc
DESKTOP-QAFHV3F\SAAD <saadtahir28@gmail.com>
2018-08-22 14:29:20 +0500

    no message

---
+++ Beat Detection/Assets/Beat Detector by Saad/MyBeat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MyBeat : MonoBehaviour
+{
+    public float initialScale;//since we need to retain our scale if no beat detected
+    public float endValue, duration;
+    Sequence s;
+    private void Start()
+    {
+        s = DOTween.Sequence();
+    }
+    public void DoBeat(float value)
+    {
+        s.Append(transform.DOScale(initialScale + value * endValue, duration).SetLoops(2, LoopType.Yoyo));
+        s.Play();
+    }
+
+
+
+
+}
---
+++ Beat Detection/Assets/Beat Detector by Saad/MyFlash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class MyFlash : MonoBehaviour {
+    public float initialFlash;
+    public float endValue, duration;
+    Sequence f;
+    private Image flash;
+    private void Start()
+    {
+        f = DOTween.Sequence();
+        flash = GetComponent<Image>();
+    }
+
+    public void DoFlash(float value)
+    {
+        f.Append(flash.DOFade(initialFlash + value * endValue, duration).SetLoops(2, LoopType.Yoyo));
+        f.Play();
+    }
+
+}
